ebef1b6d1f706d29b524f8d89d276c021f652714
LSaldyt <lucassaldyt@gmail.com>
2017-11-18 10:33:03 -0700

    Adds command line app, full reverse lookup

--- database.py
+++ database.py
@@ -1,39 +1,38 @@
 #!/usr/bin/env python3
 from collections import defaultdict
 
-def _keychain_lookup(level, keychain):
-    for key in keychain:
-        if key not in level:
-            raise KeyError('{} not in level'.format(key))
-        else:
-            level = level[key]
-    return level
-
-class MultiDictionary(object):
-
+class Database(object):
     def __init__(self):
         self.entries  = dict()
         self.mentions = defaultdict(set)
 
-    def lookup(self, keychain):
-        return _keychain_lookup(self.entries, keychain)
+    def _lookup(self, keychain):
+        level = self.entries
+        for key in keychain:
+            if key not in level:
+                raise KeyError('{} not in level'.format(key))
+            else:
+                level = level[key]
+        return level
 
-    def keychain_mentions(self, *keys):
+    def _mentions(self, *keys):
         return self.mentions[tuple(keys)]
 
-    def insert(self, *values):
+    def _insert(self, *values):
         assert len(values) > 1, 'Cannot insert without at least one key and one value'
         keychain = values[:-1]
         value    = values[-1]
 
         self.mentions[(value,)].add(tuple(keychain))
 
-        for i in range(1, len(keychain) + 1):
-            subchain = keychain[:i]
+        for i in range(1, len(values) + 1):
+            subchain = values[:i]
+            print(subchain)
             self.mentions[tuple(subchain)].add(tuple(keychain))
 
-        for i in range(len(keychain)):
-            subchain = keychain[i:]
+        for i in range(1, len(values)):
+            subchain = values[i:]
+            print(subchain)
             self.mentions[tuple(subchain)].add(tuple(keychain))
         
         for key in keychain:
@@ -49,9 +48,10 @@ class MultiDictionary(object):
         key = keychain[-1]
         level[key] = value
 
-    def ninsert(self, string):
-        self.insert(*string.split(' '))
+    def add(self, string):
+        self._insert(*string.split(' '))
+        print('Added {}'.format(string))
 
-    def nmentions(self, string):
-        return self.keychain_mentions(*string.split(' '))
-
+    def get(self, string):
+        print('Getting: {}'.format(string))
+        print(self._mentions(*string.split(' ')))
